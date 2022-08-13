8747cb477a111ffc71d89c69415dfe335507600e
unknown <honeygarg1238@gmail.com>
2022-08-13 18:55:40 +0530

    Complete counter

---
+++ Project-2-counter/app.js
@@ -0,0 +1,24 @@
+// set initial count
+
+let count = 0;
+
+//select value and buttons
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (item) => {
+    const styles = item.currentTarget.classList;
+
+    if (styles.contains("decrease")) count--;
+    else if (styles.contains("increase")) count++;
+    else count = 0;
+
+    if (count > 0) value.style.color = "green";
+    if (count < 0) value.style.color = "red";
+    if (count === 0) value.style.color = "#222";
+
+    value.textContent = count;
+  });
+});
